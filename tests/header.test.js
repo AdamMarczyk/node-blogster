641e116db9de0a73b8f0d5126ed8027c5348c9cc
Adam Marczyk <adam.marczyk90@gmail.com>
2018-08-15 08:26:47 +0200

    add test for logout

--- tests/header.test.js
+++ tests/header.test.js
@@ -50,4 +50,9 @@ test('should show logout button when signed in', async () => {
   await page.setCookie({ name: 'session', value: sessionString });
   await page.setCookie({ name: 'session.sig', value: sig });
   await page.goto('localhost:3000');
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual('Logout');
 });
